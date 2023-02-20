5e2d4d979894aef18f487a390c2421a85b7a44a9
yexiaofeng <yexiaofeng@mysnail.com>
2023-02-20 02:43:40 +0000

    project init

---
+++ im-service/src/test/java/com/yexf/imservice/ImServiceApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.yexf.imservice;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ImServiceApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
